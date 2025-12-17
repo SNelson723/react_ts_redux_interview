7f43dfafe0f6f585cc22b12e847ec53b60db6553
Stephen <stephenn649@gmail.com>
2025-12-17 09:41:12 -0600

    Sets up the empty application

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        boxSizing: "border-box",
+        padding: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>Your Task Manager here</div>
+    </div>
+  );
+};
+
+export default App;
